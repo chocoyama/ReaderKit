af1b3d89890c5724311062d40ec378afba8746e7
takyokoy <takyokoy@yahoo-corp.jp>
2016-10-27 12:39:24 +0900

    Initial Commit

---
+++ ReaderKit/ReaderKit.h
@@ -0,0 +1,19 @@
+//
+//  ReaderKit.h
+//  ReaderKit
+//
+//  Created by takyokoy on 2016/10/27.
+//  Copyright © 2016年 chocoyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReaderKit.
+FOUNDATION_EXPORT double ReaderKitVersionNumber;
+
+//! Project version string for ReaderKit.
+FOUNDATION_EXPORT const unsigned char ReaderKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReaderKit/PublicHeader.h>
+
+
